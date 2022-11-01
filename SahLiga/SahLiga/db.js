b5da5f4bdfb69aab8d183909529bc7c36d1772af
goran-brkic <93867420+goran-brkic@users.noreply.github.com>
2022-11-01 16:11:54 +0100

    Add files via upload
    Poštovani,
    
    danima sam se mučio, evo i dva čitava dana nakon predaje projekta da uspostavim na Renderu svoju aplikaciju koju sam napravio po Vašem uzoru Auth-09 ali kako 09 ima zasebno web-api, a zasebno client nikako da uspostavim da radi na renderu pa ako čitate ovo, ovo je commit da promijenim file za bazu podataka (db.js) i maknem process.env retke kako bi vam se imalo što prikazati ako pokrećete aplikaciju lokalno a ne odlučite se dati mi izravno 0 bodova.
    
    Hvala

--- SahLiga/SahLiga/db.js
+++ SahLiga/SahLiga/db.js
@@ -1,11 +1,12 @@
 const Pool = require('pg').Pool;
 
 const pool = new Pool({
-    user: "postgres",
-    password: "bazepodataka",
-    host: "localhost",
+    user: 'sahliga_user',
+    password: 'JuqlHmBfB3tyRHkQbBpBvA4i9YLuUUrL',
+    host: 'dpg-cdfg5lda4992md4hl9jg-a.frankfurt-postgres.render.com',
     port: 5432,
-    database: "SahLiga"
+    database: 'sahliga',
+    ssl: false
 });
 
 module.exports = pool;
