9789c93ff12b2d5eb28540b7c86d9f7b3ccb9086
goran-brkic <93867420+goran-brkic@users.noreply.github.com>
2022-12-14 18:27:27 +0100

    Add files via upload

--- SahLiga/SahLiga/db.js
+++ SahLiga/SahLiga/db.js
@@ -1,11 +1,15 @@
 const Pool = require('pg').Pool;
+var dotenv = require('dotenv');
+
+dotenv.config();
+
 
 const pool = new Pool({
-    user: 'sahliga_user',
-    password: 'JuqlHmBfB3tyRHkQbBpBvA4i9YLuUUrL',
-    host: 'dpg-cdfg5lda4992md4hl9jg-a.frankfurt-postgres.render.com',
+    user: process.env.RENDER_EXTERNAL_URL ? process.env.DB_USER : 'postgres',
+    password: process.env.RENDER_EXTERNAL_URL ? process.env.DB_PASSWORD : 'bazepodataka',
+    host: process.env.RENDER_EXTERNAL_URL ? process.env.DB_HOST : 'localhost',
     port: 5432,
-    database: 'sahliga',
+    database: process.env.RENDER_EXTERNAL_URL ? "sahliga" : 'SahLiga',
     ssl: true
 });
 
